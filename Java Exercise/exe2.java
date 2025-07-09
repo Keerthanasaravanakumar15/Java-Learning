8797e47a7a0d01b66fd392e6b354aa4dfa7b375e
Keerthanasaravanakumar15 <keerthanasaravanakumar15@gmail.com>
2025-07-09 11:55:35 +0530

    exe2

---
+++ Java Exercise/exe2.java
@@ -0,0 +1,9 @@
+class exe2{
+  public static void main(String[] args) {
+      System.out.println("abc 10");
+      System.out.println(20);
+      System.out.println(30);
+      System.out.print("50 The end");
+
+  }
+}
