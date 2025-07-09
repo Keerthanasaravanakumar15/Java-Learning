97ff3a60eab8f0748b6ea5e4428e7d60f805ef92
Keerthanasaravanakumar15 <keerthanasaravanakumar15@gmail.com>
2025-07-09 16:13:16 +0530

    data type

---
+++ Java Exercise/datatype.java
@@ -0,0 +1,15 @@
+public class datatype {
+  public static void main(String[] args) {
+      long num = 21474836487L;
+      System.out.println(num);
+      float a = 1.2345678f;
+      System.out.println(a);
+      double b = 1.23456789;
+      System.out.println(b);
+      char c = 'K';
+      System.out.println(c);
+      boolean d = true;
+      System.out.println(d);
+  }
+  
+}
---
+++ Java Exercise/exe3.java
@@ -0,0 +1,17 @@
+public class exe3 {
+  String name = "Keerthana S";
+  String rollnum = "22AD078"; 
+  int age = 20;
+  String dept = "AI&DS";
+  int year = 4;
+   public static void main(String[] args) {
+       exe3 detail = new exe3();
+       System.out.println("Name :" +detail.name);
+       System.out.println("Roll Number :"+detail.rollnum);
+       System.out.println("Age :"+detail.age);
+       System.out.println("Name of the department :"+detail.dept);
+       System.out.println("Year of studying :"+detail.year);
+
+   }
+  }
+
